a8b7a3dcc0584acece19377b6695aa7a22c3322e
LeoTruongMQ2020COMP1150 <leo.truong@students.mq.edu.au>
2021-09-29 17:13:32 +1000

    ScoreKeeper,UIManager

---
+++ Prac Week 8/Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour
+{
+
+    public int pointsPerPickUp;
+    public int score;
+
+    public void PickUp()
+    {
+        score += pointsPerPickUp; 
+    }
+}
