b4f196d1b6d46e014465c22ad25ce35380375039
Nicholas-Vellios <nicholas.vellios@students.mq.edu.au>
2021-09-29 19:44:05 +1000

    Changed ScoreKeeper and UIManager scripts to use a singleton pattern.

--- Prac Week 8/Assets/Scripts/ScoreKeeper.cs
+++ Prac Week 8/Assets/Scripts/ScoreKeeper.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class ScoreKeeper : MonoBehaviour
 {
+    static private ScoreKeeper instance;
+
+    static public ScoreKeeper Instance()
+    {
+        return instance;
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
 
     public int pointsPerPickUp;
-
     private int score = 0;
+
     public int Score
     {
         get
--- Prac Week 8/Assets/Scripts/UIManager.cs
+++ Prac Week 8/Assets/Scripts/UIManager.cs
@@ -9,24 +9,9 @@ public class UIManager : MonoBehaviour
     public Canvas canvas;
     private int score;
 
-    ScoreKeeper scorekeeper;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        scorekeeper = GameObject.Find("Scorekeeper").GetComponent<ScoreKeeper>();
-        canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
-        score = scorekeeper.Score;
-        canvas.gameObject.transform.GetChild(0).GetComponent<Text>().text += score;
-    }
-
-    // Update is called once per frame
     void Update()
-    {
-        if (score < scorekeeper.Score)
-        {
-            score = scorekeeper.Score;
-            canvas.gameObject.transform.GetChild(0).GetComponent<Text>().text = "Score: "+ score.ToString();
-        }
+{
+        score = ScoreKeeper.Instance().Score;
+        canvas.gameObject.transform.GetChild(0).GetComponent<Text>().text = "Score: " + score.ToString();
     }
 }
