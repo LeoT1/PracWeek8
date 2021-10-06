b9c6eaefe426a668fbaf0e8e12a833367b8f8ab2
Thi Luu <linhluu0312@users.noreply.github.com>
2021-10-06 16:43:29 +1000

    Finished coin script

--- Prac Week 8/Assets/Scripts/Coin.cs
+++ Prac Week 8/Assets/Scripts/Coin.cs
@@ -14,7 +14,7 @@ public class Coin : MonoBehaviour
         if (other.CompareTag("Player2"))
         {
             Destroy(gameObject);
-            scorekeeper.HitTwo();
+            ScoreKeeper.Instance().HitTwo();
         }
     }
 }
